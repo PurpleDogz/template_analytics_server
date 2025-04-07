c09d9863196f384ee8b0dd79bc7d5e6fac9566ab
ffma-BarnabyA <Barnaby.Alderson@futurefund.gov.au>
2025-04-07 15:15:03 +1000

    sync

--- src/data/launches.csv.js
+++ src/data/launches.csv.js
@@ -1,5 +1,13 @@
 import {csvFormat, tsvParse} from "d3-dsv";
 import {utcParse} from "d3-time-format";
+import {readFileSync} from "fs";
+import {resolve, dirname} from "path";
+
+import {fileURLToPath} from "url";
+
+// Get the current directory in ES module scope
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 async function text(url) {
   const response = await fetch(url);
@@ -29,8 +37,13 @@ const TOP_STATES_MAP = new Map([
   ["CN", "China"]
 ]);
 
-// Load and parse launch vehicles.
-const launchVehicles = tsvParse(await text("https://planet4589.org/space/gcat/tsv/tables/lv.tsv"));
+// Load and parse launch vehicles from a local TSV file.
+const tsvFilePath = resolve(__dirname, "./lv.tsv");
+const tsvContent = readFileSync(tsvFilePath, "utf-8");
+const launchVehicles = tsvParse(tsvContent);
+
+//const launchVehicles = tsvParse(await text("https://planet4589.org/space/gcat/tsv/tables/lv.tsv"));
+
 
 // Construct map to lookup vehicle family from name.
 const launchVehicleFamilyMap = new Map(launchVehicles.map((d) => [d["#LV_Name"], d.LV_Family.trim()]));
@@ -49,13 +62,26 @@ function normalizeFamily(d) {
 // Parse dates!
 const parseDate = utcParse("%Y %b %_d");
 
+// Load and parse launch vehicles from a local TSV file.
+const tsvFilePath2 = resolve(__dirname, "./launchlog.tsv");
+const tsvContent2 = readFileSync(tsvFilePath2, "utf-8");
+//const launchVehicles = tsvParse(tsvContent);
+
 // Load and parse launch-log and trim down to smaller size.
-const launchHistory = tsvParse(await text("https://planet4589.org/space/gcat/tsv/derived/launchlog.tsv"), (d) => ({
+const launchHistory = tsvParse(tsvContent2, (d) => ({
   date: parseDate(d.Launch_Date.slice(0, 11)),
   state: normalizeState(d.LVState),
   stateId: d.LVState,
   family: normalizeFamily(d.LV_Type)
 })).filter((d) => d.date != null);
+
+// const launchHistory = tsvParse(await text("https://planet4589.org/space/gcat/tsv/derived/launchlog.tsv"), (d) => ({
+//   date: parseDate(d.Launch_Date.slice(0, 11)),
+//   state: normalizeState(d.LVState),
+//   stateId: d.LVState,
+//   family: normalizeFamily(d.LV_Type)
+// })).filter((d) => d.date != null);
+
 
 // Write out csv formatted data.
 process.stdout.write(csvFormat(launchHistory));
